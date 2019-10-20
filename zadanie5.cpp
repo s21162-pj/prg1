77a3ae9da5aafc3f2adc0671ac62f9e2543600e2
Tomasz Baj <s20499@pjwstk.edu.pl>
2019-10-20 18:38:07 +0200

    zadania ze wskaźnikami

---
+++ zadanie5.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+int main()
+{
+int i;
+std::cin>>i;
+std::cout<<&i;
+return 0;
+}
---
+++ zadanie6.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int *i;
+int j=123;
+i = &j;
+cout<<j<<endl;
+cout<<*i<<endl;
+return 0;
+}
+// cout j 123
+// cout $j odres komórki
