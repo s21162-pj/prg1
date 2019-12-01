ca670b10d1bdd53735721503c0422ab88074cd7e
s21162-pj <56191739+s21162-pj@users.noreply.github.com>
2019-12-01 18:04:11 +0100

    Create zad3.cpp

---
+++ zjazd5/zad3.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+#include <ctime>
+#include <cstdlib>
+#include <cstdio>
+#include <cstddef>
+
+using namespace std;
+
+int main()
+{
+
+ vector<string> tab;
+ vector<string> tab2;
+ string klasa, imie;
+ int n;
+ cin >> n;
+ for (int i=0; i<n; ++i)
+ {
+     cin >> klasa;
+     tab.push_back(klasa);
+ }
+ for(int i=0; i<n; ++i)
+ {
+     cin>>imie;
+     tab2.push_back(imie);
+     
+ }
+ for(int i=0;i<tab.size();i++){
+     int x,y;
+     
+     x= rand() %n;
+     y= rand() %n;
+     
+     if (tab[x].empty()){
+         break;
+     }
+     else{
+         if (tab2[y].empty()){
+             break;
+         }
+         else{
+             cout<<tab[x]<<" - "<<tab2[y]<<endl;
+             tab.erase(tab.begin()+x);
+             tab2.erase(tab2.begin()+y);
+             n--;
+             
+         }
+     }
+     }
+ 
+ 
+
+    return 0;
+}
