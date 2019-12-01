0ffd4d9f2f18f28d7489909c1016d2d878a87459
s21162-pj <56191739+s21162-pj@users.noreply.github.com>
2019-12-01 17:44:50 +0100

    Create zad0.cpp

---
+++ zjazd5/zad0.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+
+    vector < int > tab;
+    for(int i=0; i<100; i++)
+   {
+       int element;
+       cin >> element;
+       if (element==0)
+   {
+       break;
+   }
+   else
+   {
+       tab.push_back(element);
+    }
+   }
+   for (int i=0;i<tab.size();i++){
+       cout<<tab[ i ]<<' ';
+       }
+
+
+    return 0;
+}
